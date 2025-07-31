66d155ee4e318a2a2d5c40deccdab546823db31e
k.amanda <katrielesteramanda@student.tudelft.nl>
2025-07-31 18:10:55 +0700

    no more completed button

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -308,21 +308,12 @@ if st.session_state.get("authenticated") and "chat_state" not in st.session_stat
         st.markdown(user_info["has_completed_presurvey"])
         st.markdown(f"""
         **1.** Click the link below to open the pre‑survey in a new tab  
-        **2.** Complete it, then come back and click **“I completed it!”**
+        **2.** Complete it, then come back and refresh this page!”**
+        *Note: If you would like to add more tasks, don't worry, you could always come back to this chatbot after Pre-Survey to add more (up to 3)!*
 
         [🚀 Open Pre‑Survey here]({survey_url})  
         """)
-        
-        if st.button("✅ I completed it!"):
-            components.html(
-                """
-                <script>
-                window.location.reload(true);
-                </script>
-                """,
-                height=0,
-                width=0,
-            )
+    
         if "chat_thread" not in st.session_state:
             st.session_state["chat_thread"] = ChatThread(st.session_state["user_id"])
 
